94803a21d4867369ea222366d9dc030217224862
zaneli <shun.otani@gmail.com>
2014-04-28 04:13:04 +0000

    Restore Travis CI configuration file, and add setup script.

---
+++ src/test/resources/setup_travis.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Install ZeroMQ
+wget http://download.zeromq.org/zeromq-4.0.4.tar.gz
+tar -xzf zeromq-4.0.4.tar.gz
+cd zeromq-4.0.4
+./configure
+make
+sudo make install
+cd ../
+
+# Install JZMQ
+git clone https://github.com/zeromq/jzmq.git
+cd jzmq
+./autogen.sh
+./configure
+make
+sudo make install
+cd ../
+
+# Replace jar file
+rm -f lib/zmq.jar
+cp /usr/local/share/java/zmq.jar lib/
